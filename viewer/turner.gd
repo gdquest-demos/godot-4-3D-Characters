8838c54b4a1e9d805686b772366d4266fb3a75f0
Tibo <goiffonthibaud@protonmail.com>
2023-01-21 13:41:26 +0100

    add: zoom with mouse wheel

--- viewer/turner.gd
+++ viewer/turner.gd
@@ -2,7 +2,9 @@ extends Node3D
 
 @export var low_angle : float = -5.0
 @export var high_angle : float = -50.0
-
+@export var min_zoom : float = 1.5
+@export var max_zoom : float = 5.0
+@onready var _camera = $Camera3D
 var _is_grabbing = false
 
 func _ready():
@@ -11,9 +13,8 @@ func _ready():
 	
 	
 func _set_bg_ratio():
-	var camera = $Camera3D
 	var background_plane : MeshInstance3D = $BackgroundPlane
-	var frustum_height = tan(camera.fov * PI / 180 * 0.5) * (camera.position.z - background_plane.position.z) * 2
+	var frustum_height = tan(_camera.fov * PI / 180 * 0.5) * (_camera.position.z - background_plane.position.z) * 2
 	var viewport_size = get_viewport().size
 	var ratio = float(viewport_size.x) / float(viewport_size.y)
 
@@ -22,12 +23,22 @@ func _set_bg_ratio():
 	background_plane.material_override.set_shader_parameter("ratio", Vector2(ratio, 1.0))
 
 func _unhandled_input(event):
+	var wheel_direction = 0.0
 	if (event is InputEventMouseButton): 
-		_is_grabbing = event.pressed
-	# Check mouse motion
-	if !(event is InputEventMouseMotion): return
-	if !_is_grabbing: return
+		if event.button_index == MOUSE_BUTTON_LEFT: _is_grabbing = event.pressed
+		var wheel_up = event.button_index == MOUSE_BUTTON_WHEEL_UP
+		var wheel_down = event.button_index == MOUSE_BUTTON_WHEEL_DOWN
+		
+		wheel_direction = float(wheel_down) - float(wheel_up)
 	
-	rotation.y += -event.relative.x * 0.005
-	rotation.x += -event.relative.y * 0.005
-	rotation.x = clamp(rotation.x, deg_to_rad(high_angle), deg_to_rad(low_angle))
+	if wheel_direction != 0:
+		_camera.position.z += wheel_direction * 0.25
+		_camera.position.z = clamp(_camera.position.z, min_zoom, max_zoom)
+		_set_bg_ratio()
+		
+	# Check mouse motion
+	if (event is InputEventMouseMotion):
+		if !_is_grabbing: return
+		rotation.y += -event.relative.x * 0.005
+		rotation.x += -event.relative.y * 0.005
+		rotation.x = clamp(rotation.x, deg_to_rad(high_angle), deg_to_rad(low_angle))
--- viewer/ui/model_selector/bubble_scroll.gd
+++ viewer/ui/model_selector/bubble_scroll.gd
@@ -12,7 +12,8 @@ func setup():
 		tag.pivot_offset.y = tag.size.y / 2
 
 func _gui_input(event):
-	if !event.is_pressed(): return
+	if !(event is InputEventMouseButton): return
+	accept_event()
 	var wheel_up = event.button_index == MOUSE_BUTTON_WHEEL_UP
 	var wheel_down = event.button_index == MOUSE_BUTTON_WHEEL_DOWN
 	
@@ -23,17 +24,8 @@ func _gui_input(event):
 	
 	var t = create_tween()
 	t.tween_property(v_box, "position:y", goal_position, 0.1)
-	# t.tween_method(set_scroll, v_box.position.y, goal_position, 0.1)
 	
 func _get_item_height():
 	var box_sep = v_box.get("theme_override_constants/separation")
 	var tag_height = v_box.get_child(0).size.y
 	return (tag_height + box_sep)
-
-func set_scroll(pos_y):
-	v_box.position.y = pos_y
-	var half_height = custom_minimum_size.y / 2.0
-	for tag_index in v_box.get_child_count():
-		var dist = abs((tag_index * _get_item_height()) - (-pos_y))
-		dist = clamp(dist, 0.0, half_height)
-		v_box.get_child(tag_index).scale = Vector2.ONE * remap(dist, 0.0, half_height, 1.0, 0.8)
