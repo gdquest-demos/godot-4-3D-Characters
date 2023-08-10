e5bebc9dee14e5ada0b3a7d1ef230a8ce77f4b41
Tibo <goiffonthibaud@protonmail.com>
2023-08-10 14:05:43 +0200

    update: sophia model

--- models/sophia/sophia_skin.gd
+++ models/sophia/sophia_skin.gd
@@ -1,110 +1,54 @@
 extends Node3D
 
-## Represents the blending between the walking and running animations. It can be set to different values (e.g. 0.0 to 1.0) to adjust the balance between the two animations, resulting in the model appearing to walk or run depending on the value.
-var walk_run_blending = 0.0 : set = _set_walk_run_blending
+@onready var animation_tree = %AnimationTree
+@onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
+@onready var move_tilt_path : String = "parameters/StateMachine/Move/tilt/add_amount"
 
-# Set loop on some animation
-@export var _force_loop : PackedStringArray
-@onready var _animation_tree = $AnimationTree
-@onready var _main_state_machine : AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/StateMachine/playback")
-@onready var _second_state_machine : AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/StateMachine/StateMachine/playback")
-@onready var _walk_run_blend_position : String = "parameters/StateMachine/StateMachine/Walk/blend_position"
+var run_tilt = 0.0 : set = _set_run_tilt
 
-@onready var _model_mesh : MeshInstance3D = $mini_sophia/Armature/Skeleton3D/sophia_mesh
-@onready var _eye_left_mat : BaseMaterial3D = _model_mesh.get("surface_material_override/1")
-@onready var _eye_right_mat : BaseMaterial3D = _model_mesh.get("surface_material_override/2")
-@onready var _mouth_mat : BaseMaterial3D = _model_mesh.get("surface_material_override/3")
-
-
-var _blinking = null : set = _set_blinking
-@onready var _blinking_timer : Timer = $BlinkTimer
-@onready var _closed_eyes_timer : Timer = $ClosedTimer
-var _eyes_atlas_order = ["default", "closed", "surprised", "worried", "angry"]
-var _eyes_atlas = {}
-var _current_eyes = "default" : set = _set_current_eyes
-var _mouth_atlas_order = ["default", "happy", "surprised", "frowning", "disgust"]
-var _mouth_atlas = {}
+@export var blink = true : set = set_blink
+@onready var blink_timer = %BlinkTimer
+@onready var closed_eyes_timer = %ClosedEyesTimer
+@onready var eye_mat = $sophia/rig/Skeleton3D/Sophia.get("surface_material_override/2")
 
 func _ready():
-	for animation_name in _force_loop:
-		var anim : Animation = $mini_sophia/AnimationPlayer.get_animation(animation_name)
-		anim.loop_mode = Animation.LOOP_LINEAR
-	# Setup eyes
-	_eyes_atlas = _get_atlas(_eyes_atlas_order)
-	_mouth_atlas = _get_atlas(_mouth_atlas_order)
-	_blinking_timer.connect("timeout", _on_blink_timer_timeout)
-	_set_blinking(true)
-	
-func _get_atlas(base_atlas):
-	var atlas = {}
-	var step = 1.0 / base_atlas.size()
-	for atlas_index in base_atlas.size():
-		atlas[base_atlas[atlas_index]] = step * atlas_index
-	return atlas
-	
-func _set_walk_run_blending(value : float):
-	walk_run_blending = value
-	_animation_tree.set(_walk_run_blend_position, walk_run_blending)
-
-## Sets the model to a neutral, action-free state.
-func idle():
-	_main_state_machine.travel("StateMachine")
-	_second_state_machine.travel("Idle")
-
-## Sets the model to a walking or running animation or forward movement.
-func walk():
-	_main_state_machine.travel("StateMachine")
-	_second_state_machine.travel("Walk")
-	
-## Sets the model to an upward-leaping animation, simulating a jump.
-func jump():
-	_main_state_machine.travel("StateMachine")
-	_second_state_machine.travel("Jump")
-	
-## Sets the model to a downward animation, imitating a fall.
-func fall():
-	_main_state_machine.travel("StateMachine")
-	_second_state_machine.travel("Fall")
-
-## Sets the model to an edge-grabbing animation.
-func edge_grab():
-	_main_state_machine.travel("EdgeGrab")
-
-## Sets the model to a wall-sliding animation.
-func wall_slide():
-	_main_state_machine.travel("WallSlide")
-
-func _set_blinking(value : bool):
-	_blinking = value
-	if _blinking:
-		_blinking_timer.start()
-	else:
-		_blinking_timer.stop()
-		_set_eyes(_current_eyes)
-
-func _on_blink_timer_timeout():
-	# Close eyes
-	_set_eyes("closed")
-	_closed_eyes_timer.start(randf_range(0.1, 0.25))
-	await _closed_eyes_timer.timeout
-	# Return to current eyes
-	_set_eyes(_current_eyes)
-	if randf_range(0.0, 1.0) > 0.8:
-		_blinking_timer.wait_time = randf_range(0.1, 0.15)
-	else:
-		_blinking_timer.wait_time = randf_range(1.0, 4.0)
-	_blinking_timer.start()
-
-func _set_current_eyes(value : String):
-	_current_eyes = value
-	_set_eyes(value)
-	_set_blinking(value == "default")
+	blink_timer.connect("timeout", func():
+		eye_mat.set("uv1_offset", Vector3(0.0, 0.5, 0.0))
+		closed_eyes_timer.start(0.2)
+		)
 		
-func _set_eyes(eyes_name : String):
-	var texture_offset = _eyes_atlas[eyes_name]
-	_eye_left_mat.set("uv1_offset", Vector3(0.0, texture_offset, 0.0))
-	_eye_right_mat.set("uv1_offset", Vector3(0.0, texture_offset, 0.0))
-	
-func _set_mouth(mouth_name : String):
-	var texture_offset = _mouth_atlas[mouth_name]
-	_mouth_mat.set("uv1_offset", Vector3(0.0, texture_offset, 0.0))
+	closed_eyes_timer.connect("timeout", func():
+		eye_mat.set("uv1_offset", Vector3.ZERO)
+		blink_timer.start(randf_range(1.0, 4.0))
+		)
+
+func set_blink(state : bool):
+	if blink == state: return
+	blink = state
+	if blink:
+		blink_timer.start(0.2)
+	else:
+		blink_timer.stop()
+		closed_eyes_timer.stop()
+
+func _set_run_tilt(value : float):
+	run_tilt = clamp(value, -1.0, 1.0)
+	animation_tree.set(move_tilt_path, run_tilt)
+
+func idle():
+	state_machine.travel("Idle")
+
+func move():
+	state_machine.travel("Move")
+
+func fall():
+	state_machine.travel("Fall")
+
+func jump():
+	state_machine.travel("Jump")
+
+func edge_grab():
+	state_machine.travel("EdgeGrab")
+
+func wall_slide():
+	state_machine.travel("WallSlide")
