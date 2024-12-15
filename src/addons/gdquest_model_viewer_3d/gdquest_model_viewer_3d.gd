4b070a4883b40ca6e7ddd569cf3fd2fd84685d62
xananax <me@xananax.com>
2024-12-15 15:13:17 +0100

    feat: allow to switch characters from the command line or web hash

--- src/addons/gdquest_model_viewer_3d/gdquest_model_viewer_3d.gd
+++ src/addons/gdquest_model_viewer_3d/gdquest_model_viewer_3d.gd
@@ -5,9 +5,61 @@ extends Node
 const _ModelViewer3d = preload("./viewer/model_viewer_3d.gd")
 const _MODEL_VIEWER_3D_SCENE = preload("./viewer/model_viewer_3d.tscn")
 
+signal children_ready
+
+var _data_models_map := {}
+var _scene: _ModelViewer3d
+var _children_are_ready := false
+
 @export var known_models : Array[ModelData]
 
 func _ready() -> void:
-	var scene := _MODEL_VIEWER_3D_SCENE.instantiate() as _ModelViewer3d
-	scene.known_models = known_models
-	add_child(scene)
+	_scene = _MODEL_VIEWER_3D_SCENE.instantiate() as _ModelViewer3d
+	_scene.known_models = known_models
+	add_child(_scene)
+	
+	for index in known_models.size():
+		var model := known_models[index]
+		var model_name := model.name.to_lower().replace(" ", "_")
+		_data_models_map[model_name] = index
+	
+	# allow the sie menu to get its size
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	_children_are_ready = true
+	children_ready.emit()
+	
+	set_model_from_arguments()
+	set_model_from_browser_hash()
+
+
+func set_model_by_name(model_name: String) -> void:
+	if not (model_name in _data_models_map):
+		return
+	if not _children_are_ready:
+		await children_ready
+	_scene.model_selector.selection = _data_models_map[model_name]
+
+
+func set_model_from_arguments() -> void:
+	var arguments := {}
+	for argument in OS.get_cmdline_args():
+		if argument.find("=") > -1:
+			var key_value := argument.split("=")
+			arguments[key_value[0].lstrip("--")] = key_value[1]
+	if 'model' in arguments:
+		set_model_by_name(arguments['model'])
+
+
+func set_model_from_browser_hash() -> void:
+	if not OS.has_feature('web'):
+		return
+	var hash_bits = (JavaScriptBridge.eval("window.location.hash") as String).lstrip('#').split("&")
+	var arguments := {}
+	for hash in hash_bits:
+		if hash.find('=') > -1:
+			var key_value := hash.split("=")
+			arguments[key_value[0]] = key_value[1]
+	if 'model' in arguments:
+		set_model_by_name(arguments['model'])
+			
--- src/addons/gdquest_model_viewer_3d/viewer/ui/model_selector/model_selector.gd
+++ src/addons/gdquest_model_viewer_3d/viewer/ui/model_selector/model_selector.gd
@@ -5,8 +5,8 @@ extends VBoxContainer
 @onready var scroll_container = %ScrollContainer
 @export var model_tag_scene : PackedScene
 
-var selection = 0
-var model_list 
+var selection := 0: set = set_selection
+var model_list: Array[ModelData]
 
 var _is_open = false : set = _set_is_open
 
@@ -21,7 +21,7 @@ func setup(new_model_list : Array[ModelData]):
 		var tag = model_tag_scene.instantiate()
 		tag.tag_name = model.name
 		tag.icon_texture = model.vignette
-		tag.connect("pressed", tag_pressed.bind(model_index))
+		tag.connect("pressed", set_selection.bind(model_index))
 		tag_list.add_child(tag)
 		
 	await get_tree().process_frame
@@ -29,13 +29,15 @@ func setup(new_model_list : Array[ModelData]):
 	scroll_container.setup()
 	_set_is_open(false)
 	
-func tag_pressed(index : int):
-	if selection == index: return
+func set_selection(index : int):
+	if selection == index  or selection < 0 or selection >= model_list.size(): 
+		return
 	selection = index
 	_set_is_open(false)
 	model_tag.tag_name = model_list[index].name
 	model_tag.icon_texture = model_list[index].vignette
 	emit_signal("selection_changed", index)
+
 
 func _on_model_tag_pressed():
 	_set_is_open(!_is_open)
