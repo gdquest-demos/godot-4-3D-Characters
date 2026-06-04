020a506a89ba98cd4ceaaaf0271d6fed28c507b7
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2026-06-04 20:18:59 +0300

    chore: showcase all available animations

--- src/addons/gdquest_round_bat/round_bat_skin.gd
+++ src/addons/gdquest_round_bat/round_bat_skin.gd
@@ -4,7 +4,12 @@ extends Node3D
 @onready var _hurt_shot_path: String = "parameters/HurtShot/request"
 
 
+## Sets the model to a neutral, action-free state.
+func idle() -> void:
+	_animation_tree.set(_hurt_shot_path, AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+
+
 ## Play a OneShot hurt animation.
 ## This animation is played in paralel with other states.
 func hurt() -> void:
-	_animation_tree.set(_hurt_shot_path, true)
+	_animation_tree.set(_hurt_shot_path, AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
